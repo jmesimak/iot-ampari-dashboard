274c682d971164e1537972cac65c514e311772d7
Felix Lindholm <felix.lindholm@gmail.com>
2016-06-14 08:57:35 +0300

    very nice graphs and apple

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,16 +21,6 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .when('/charts', {
         templateUrl: 'views/charts.html',
         controller: 'ChartTestCtrl'
       })
--- app/scripts/controllers/chart.js
+++ app/scripts/controllers/chart.js
@@ -10,10 +10,24 @@
 angular.module('ghIotDashboardApp')
   .controller('ChartTestCtrl', ['$scope', function ($scope) {
 
-  	$scope.labels = ["10:00", "11:00", "12:00", "13:00", "14:00", "15:00"];
-  	$scope.series = ["Jonnepeber"]
-  	$scope.data = [
+  	$scope.moistureLabels = ["10:00", "11:00", "12:00", "13:00", "14:00", "15:00"];
+  	$scope.moistureSeries = ["Moisture"]
+  	$scope.moistureData = [
   		[500, 475, 455, 420, 390, 360]
   	];
 
+    $scope.temperatureLabels = ["10.00", "11.00", "12.00", "13.00", "14.00", "15.00"];
+  	$scope.temperatureSeries = ["Temperature"]
+  	$scope.temperatureData = [
+  		[24, 23, 21, 19, 22, 20]
+  	];
+
+    var statusList = [
+      {status: "critical", text: "RIP"},
+      {status: "warning", text: "pls water me"},
+      {status: "good", text: "all is good"}
+    ];
+
+    $scope.status = statusList[0];
+    $scope.estimation = 8;
   }]);
